622bdf94da2e6e0e7b8a354e375f3996b176e5d9
Emil Thorenfeldt <emt@magenta-aps.dk>
2019-07-12 17:02:07 +0200

    Add routes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,28 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, HttpRequest};
+
+fn list_books() -> impl Responder {
+    HttpResponse::Ok().content_type("text/html; charset=utf-8").body("Køb nu bare den fucking bog, for fanden.")
+}
+
+fn upload() -> impl Responder {
+
+}
+
+fn get_book(req: HttpRequest) -> impl Responder {
+    let book_id = req.match_info().get("id").unwrap();
+    format!("Here is book {}", &book_id)
+}
 
 fn index() -> impl Responder {
-    HttpResponse::Ok().content_type("text/html; charset=utf-8").body("Køb nu bare den fucking bog, for fanden.")
+
 }
 
 fn main() {
     HttpServer::new(|| {
-        App::new().route("/", web::get().to(index))
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/books", web::get().to(list_books))
+            .route("/books", web::post().to(upload))
+            .route("/books/{id}", web::get().to(get_book))
     }).bind("127.0.0.1:8123").unwrap().run().unwrap();
 }
